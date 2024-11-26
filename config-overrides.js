071a7035ecc21d1a55066a4442444a9a2ab5e5cb
Bar Ben Haim <barbenbh@gmail.com>
2024-11-26 18:50:05 +0200

    deployment test

--- config-overrides.js
+++ config-overrides.js
@@ -2,8 +2,7 @@ const path = require('path')
 
 module.exports = {
     paths: function (paths, env) {
-        // Set the build output directory to `../backend/public`
-        paths.appBuild = path.resolve(__dirname, '../moveo-backend/public')
+        paths.appBuild = path.resolve(__dirname, '../backend/public') // Ensure paths align with backend
         return paths
     },
 }
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,11 +1,13 @@
 import axios from 'axios'
 
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || ''
+
 export const fetchCodeBlocks = async () => {
-    const response = await axios.get('/api/codeblocks')
+    const response = await axios.get(`${API_BASE_URL}/api/codeblocks`)
     return response.data
 }
 
 export const fetchCodeBlock = async id => {
-    const response = await axios.get(`/api/codeblocks/${id}`)
+    const response = await axios.get(`${API_BASE_URL}/api/codeblocks/${id}`)
     return response.data
 }
